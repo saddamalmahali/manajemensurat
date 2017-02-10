68886e5a2138b7eeaed9a07b1faacb2d057b088e
Saddam Almahali <saddamalmahali@yahoo.co.id>
2017-02-10 15:48:54 +0700

    perbaikan login menggunakan username

--- database/seeds/UserSeed.php
+++ database/seeds/UserSeed.php
@@ -12,15 +12,9 @@ class UserSeed extends Seeder
     public function run()
     {
         $jafar = new User();
-        $jafar->name = "Japar Sidik";
-        $jafar->email = "1306074@sttgarut.ac.id";
+        $jafar->name = "Admin Aplikasi";
+        $jafar->username = "admin";
         $jafar->password = bcrypt('admin');
         $jafar->save();
-
-		$fahrul = new User();
-        $fahrul->name = "Fahrul Siddik";
-        $fahrul->email = "1306048@sttgarut.ac.id";
-        $fahrul->password = bcrypt('admin');
-        $fahrul->save();        
     }
 }
